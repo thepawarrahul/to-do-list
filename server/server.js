a2d36aaa07a1b1bc85c65d3097adb1792c17d700
Rahul Pawar <pawarrahul.em@gmail.com>
2024-01-06 21:14:05 +0530

    User Registration end point

--- server/server.js
+++ server/server.js
@@ -1,13 +1,37 @@
 import express from 'express';
 import 'dotenv/config';
+import fs from 'fs/promises';
 
-const server = express()
+const userDataFileLocation = './database/userData.json';
 
+const server = express();
 const serverPort = process.env.SERVER_PORT || 4000;
 
-server.get('/', function (req, res) {
-  res.send('To do list server')
-})
+server.use(express.json());
+
+server.get('/' ,(req, res) => {
+    res.send('To do list server');
+});
+
+server.post('/registerUser', async (req, res) => {
+    // TODO 
+    // 1. Add logic to avoid user with same username.
+    // 2. Refactor the code to modules.
+    const userData = await fs.readFile(userDataFileLocation, 'utf8');
+    const userDataJson = JSON.parse(userData);
+
+    const user = {
+        id : new Date().getTime(),
+        username : req.body.username,
+        active : true,
+        task : []
+    };
+
+    userDataJson.push(user);
+    await fs.writeFile(userDataFileLocation, JSON.stringify(userDataJson, null, 2));
+
+    res.send(`User Registered With id ${user.id} and username ${user.username}`);
+});
 
 console.log('Starting server on PORT : '+serverPort);
 server.listen(serverPort);
