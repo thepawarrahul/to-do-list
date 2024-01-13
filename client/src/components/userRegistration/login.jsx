14a8951cd92b08dafb6c4a9361ba21eb572257fb
Rahul Pawar <pawarrahul.em@gmail.com>
2024-01-14 00:23:05 +0530

    login component with base code

--- client/src/components/userRegistration/login.jsx
+++ client/src/components/userRegistration/login.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import ToDo from "../todo/todo";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleLoginButtonClick = () => {
+    if (username === 'abc') {
+      setIsLoggedIn(true);
+    }
+  }; 
+
+  if (isLoggedIn) {
+    return <ToDo />;
+  }
+
   return (
     <>
-      <p>This is loging component !</p>
+      <div>
+        <div>
+          <input 
+            type='text' 
+            placeholder="username"
+            value={username}
+            onChange={handleUsernameChange}
+            >
+          </input>
+        </div>
+        <div>
+          <button 
+            onClick={handleLoginButtonClick}
+          >
+            Login
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ToDo from './components/todo/todo';
+import Login from './components/userRegistration/login';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ToDo />
+    <Login />
   </React.StrictMode>,
 )
