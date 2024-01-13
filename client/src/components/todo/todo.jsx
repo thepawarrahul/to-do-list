c513a9846fa10586bcc5aeb51df1464de9ea8528
Rahul Pawar <pawarrahul.em@gmail.com>
2024-01-14 01:00:44 +0530

    Todo component base code.

--- client/src/components/todo/todo.jsx
+++ client/src/components/todo/todo.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ToDo = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskToAdd, setTaskToAdd] = useState("");
+
+  const handleTaskAddChange = (event) => {
+    setTaskToAdd(event.target.value);
+  };
+
+  const handAddTask = () => {
+    const newTaskToAdd = {
+      id : Date.now(),
+      task: taskToAdd,
+      completed: false
+    };
+
+    setTasks([...tasks, newTaskToAdd]);
+    setTaskToAdd("");
+  };
+
   return (
     <>
-      <p>This is todo component !!</p>
+      <div>
+        <input 
+          type='text' 
+          placeholder="task"
+          onChange={handleTaskAddChange}
+        />
+      </div>
+      <div>
+        <button onClick={handAddTask}>Add Task</button>
+      </div>
+      <div>
+        <ul style={{ listStyleType: 'none' }}>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <input
+                type="checkbox"
+                checked={task.completed}
+              />
+              {task.task}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
